928974754cc96c640251207476ce768b721400f3
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2024-07-10 11:42:50 -0300

    bug in personasfield

--- src/components/Form/Modulos-Reutilizables/PersonasField.jsx
+++ src/components/Form/Modulos-Reutilizables/PersonasField.jsx
@@ -5,22 +5,22 @@ const { Option } = Select;
 
 const PersonasField = () => {
     const numeroAPalabra = (numero) => {
-        const unidades = ['', 'un', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve'];
-        const decenas = ['diez', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
-        const centenas = ['ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos'];
+        const unidades = ['', 'UN', 'DOS', 'TRES', 'CUATRO', 'CINCO', 'SEIS', 'SIETE', 'OCHO', 'NUEVE'];
+        const decenas = ['DIEZ', 'VEINTE', 'TREINTA', 'CUARENTA', 'CINCUENTA', 'SESENTA', 'SETENTA', 'OCHENTA', 'NOVENTA'];
+        const centenas = ['CIENTO', 'DOSCIENTOS', 'TRESCIENTOS', 'CUATROCIENTOS', 'QUINIENTOS'];
 
-        if (numero === 100) return 'cien';
+        if (numero === 100) return 'CIEN';
         if (numero <= 9) return unidades[numero];
-        if (numero <= 29) return decenas[Math.floor(numero / 10) - 1] + (numero % 10 !== 0 ? ' y ' + unidades[numero % 10] : '');
-        if (numero <= 99) return decenas[Math.floor(numero / 10) - 1] + (numero % 10 !== 0 ? ' y ' + unidades[numero % 10] : '');
+        if (numero <= 29) return decenas[Math.floor(numero / 10) - 1] + (numero % 10 !== 0 ? ' Y ' + unidades[numero % 10] : '');
+        if (numero <= 99) return decenas[Math.floor(numero / 10) - 1] + (numero % 10 !== 0 ? ' Y ' + unidades[numero % 10] : '');
         
         const centena = Math.floor(numero / 100);
         const resto = numero % 100;
         let resultado = centenas[centena - 1];
         if (resto !== 0) {
             if (resto <= 9) resultado += ' ' + unidades[resto];
-            else if (resto <= 29) resultado += ' ' + decenas[Math.floor(resto / 10) - 1] + (resto % 10 !== 0 ? ' y ' + unidades[resto % 10] : '');
-            else resultado += ' ' + decenas[Math.floor(resto / 10) - 1] + (resto % 10 !== 0 ? ' y ' + unidades[resto % 10] : '');
+            else if (resto <= 29) resultado += ' ' + decenas[Math.floor(resto / 10) - 1] + (resto % 10 !== 0 ? ' Y ' + unidades[resto % 10] : '');
+            else resultado += ' ' + decenas[Math.floor(resto / 10) - 1] + (resto % 10 !== 0 ? ' Y ' + unidades[resto % 10] : '');
         }
         return resultado;
     };
