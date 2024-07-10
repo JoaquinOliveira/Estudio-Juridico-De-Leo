b91bbe1c5f94b921723d7e17928ae0413adbd895
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2024-07-10 11:26:36 -0300

    upper letters a DemandadoField

--- src/components/Form/Modulos-Reutilizables/DemandadoField.jsx
+++ src/components/Form/Modulos-Reutilizables/DemandadoField.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Form, Input } from 'antd';
 
-const DemandadoField = () => (
-    <>
-        <Form.Item
-            label="Nombre del Demandado"
-            name="nombreDemandado"
-            rules={[{ required: true, message: 'Por favor ingrese el nombre del demandado' }]}
-        >
-            <Input placeholder="Ingrese el nombre del demandado" />
+const DemandadoField = () => {
+    const handleUppercase = (e, form) => {
+        const { name, value } = e.target;
+        form.setFieldsValue({ [name]: value.toUpperCase() });
+    };
+
+    return (
+        <Form.Item noStyle shouldUpdate>
+            {(form) => (
+                <>
+                    <Form.Item
+                        label="Nombre del Demandado"
+                        name="nombreDemandado"
+                        rules={[{ required: true, message: 'Por favor ingrese el nombre del demandado' }]}
+                    >
+                        <Input
+                            placeholder="Ingrese el nombre del demandado"
+                            onChange={(e) => handleUppercase(e, form)}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Domicilio del Demandado"
+                        name="domicilioDemandado"
+                        rules={[{ required: true, message: 'Por favor ingrese el domicilio del demandado' }]}
+                    >
+                        <Input
+                            placeholder="Ingrese el domicilio del demandado"
+                            onChange={(e) => handleUppercase(e, form)}
+                        />
+                    </Form.Item>
+                </>
+            )}
         </Form.Item>
-        <Form.Item
-            label="Domicilio del Demandado"
-            name="domicilioDemandado"
-            rules={[{ required: true, message: 'Por favor ingrese el domicilio del demandado' }]}
-        >
-            <Input placeholder="Ingrese el domicilio del demandado" />
-        </Form.Item>
-    </>
-);
+    );
+};
 
 export default DemandadoField;
